8ec4525ad633869c3c309eb21aad84e78120d55f
ViCastorino <vih.teodoro16@gmail.com>
2019-07-18 02:34:30 -0300

    modified

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-let quadrados = document.querySelectorAll(".caixa");
-let imagens = [ 'homiranha','menino','vilao','tiazinha','menina','porquinho'];
-
-
-function aleatorio(){
-    let min = 0;
-    let  max = imagens.length -1;
-    let i = Math.floor(Math.random(min, max) * (max - min + 1) + min);
-    return `Imagens/${imagens[i]}.PNG`;
- 
- }
-
-
-console.log(quadrados);
-
 function mostrar(event){
     console.log('a')
 }
 
 
+  // CARO INTERPRETADOR RODA ESSE CÓDIGO PELOAMORDEDEUS
 
-function selectionarCarta(event){
-    let selecionadas = Array.fill(null)
-    selectionadas = event.target
-}
-
-
+  let cardList = document.querySelectorAll('.caixa')
+  let selecionados = [];
+  let i = 0
+  
+  
+  function saoIguais(vetor){
+    console.log(vetor)
+  }
+  
+  //RETIRAR O FILTRO QUE ESCURECE A IMG
+  function mostrarCard(ev) {
+      ev.target.classList.toggle('showCard');
+  }
+  
+  //ARMAZENAR OS DOIS ELEMENTOS CLICADOS E VERIFICA-LOS
+  function selecionarCard(ev){
+  
+    selecionados[i] = ev.target.src
+    i+=1;
+    console.log(i , selecionados)
+  }
+  
+  //EVENTOS DE CLIQUE PARA A CLASSE .CARD
+  for (let card of cardList) {
+    card.onclick = function(){
+    mostrarCard(event);
+    selecionarCard(event);
+  
+    }
+  }
 
 for(let quadrado of quadrados){
     quadrado.onclick = function(event){
@@ -34,4 +44,29 @@ for(let quadrado of quadrados){
     };
 }
 
-aleatorio();
+ //aleatorio();    
+//      let imgs = ["homiranha.png","menina.png","porquinho.png","tiazinha.png","menino.png","vilao.png"];
+//      function aleatorio(lista){
+//      let valor;
+//      let indice;   
+//      let min = 0;
+//      let  max = imgs.length -1;
+//      for(let i = lista.length - 1){
+//      indice = Math.floor(Math.random(min, max) * (max - min + 1) + min);
+//      valor = lista[i]; 
+//      lista[i] = lista[indice]; 
+//      lista[indice] = valor;
+//      }
+//      return lista;
+//   }
+
+
+
+
+
+
+
+
+
+
+
